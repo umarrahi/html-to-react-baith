a6c9387d6a8b09a86abf169f73bf5798af94ef86
Umar Rahi <rahiaptech@gmail.com>
2025-07-29 19:50:01 +0500

    Update

--- src/components/FAQ.tsx
+++ src/components/FAQ.tsx
@@ -1,11 +1,10 @@
-import React from 'react'
 
 const FAQ = () => {
     return (
         <section className="bg-white dark:bg-gray-900">
             <div className="mx-auto max-w-screen-xl px-4 py-8 sm:py-16 lg:px-6">
                 <h2 className="mb-8 text-4xl font-extrabold tracking-tight text-gray-900 dark:text-white">
-                    Frequently asked questions
+                    Frequently asked questions with
                 </h2>
                 <div className="grid border-t border-gray-200 pt-8 text-left md:grid-cols-2 md:gap-16 dark:border-gray-700">
                     <div>
